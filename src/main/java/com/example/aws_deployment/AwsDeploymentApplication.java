b7199812f08607397c8c25548bf8223855ca76d9
saranyellanki <saran.yellanki12@gmail.com>
2025-07-08 12:34:01 +0530

    AWS delpoyment test

---
+++ src/main/java/com/example/aws_deployment/AwsDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aws_deployment/AwsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
